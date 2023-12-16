9d28eead4cd5db5a7c30cc10f7916aa45f643ddd
I.Safonov <safonovil17@yandex.ru>
2023-12-16 15:09:54 +0300

    Added base structure

---
+++ src/home/Home.tsx
@@ -0,0 +1,16 @@
+import styles from './Home.module.css'
+
+function Home() {
+	return (
+		<div className={styles.wrapper}>
+			<form>
+				<h1>GTA 6 - Оставь заявку</h1>
+				<input type='name' placeholder='Введите имя' />
+				<input type='email' placeholder='Введите email' />
+				<button>Хочу GTA</button>
+			</form>
+		</div>
+	)
+}
+
+export default Home
